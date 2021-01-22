a2ee75a4eb247cdcdbcdd0caf0669c7b05b47034
Amine.mouad <amouad@umanis.com>
2021-01-22 11:22:57 +0100

    Adding my introduction

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,25 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>
+          I am Mouad Amine, 23 years old, from Casablanca. I am passionate about
+          modern technologies, especially web and mobile development, I spend my
+          free time reading. I hold an engineering degree in computer methods
+          applied to business management at the Moroccan School of Engineering
+          Sciences, I am currently working as a full-stack developer at Umanis
+          since February, my mission was to develop a mobile application for a
+          Moroccan client with the framework React Native for the front end and
+          Java 7 with implementation Jersey and specification Jax RS for the
+          backend. This experience allowed me to acquire reflexes of debugging
+          and finding solutions for the problems faced during development. I
+          also worked with some REST-APIs provided by the client to accelerate
+          the development, as a version of control we used GitLab using the tool
+          Git, and we used Agile as a development methodology because the client
+          did not have a clear vision of the application, which is why we
+          adopted this methodology .. so the project was split in sprints to
+          avoid feedback and discuss with the customer the next features to be
+          developed. ]
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
